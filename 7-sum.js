8345c08be640d6618485aa66de70d8c7ddf8b9cb
MostafizurSawon <MostafizurSawon@users.noreply.github.com>
2022-06-14 01:21:28 +0600

    Total 7 added

---
+++ 7-sum.js
@@ -0,0 +1,21 @@
+const numbers = [44, 23, 34, 32, 54, 5, 78];
+
+
+// let sum = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//     const element = numbers[i];
+//     sum = sum + element;
+// }
+// console.log(sum);
+
+function arrayTotal(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        const element = numbers[i];
+        sum = sum + element;
+    }
+    return sum;
+}
+
+const total = arrayTotal(numbers);
+console.log('array total', total)
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # problem-solving-javascript
+
+1. Facorial calulation problem. (1-2, 1-3, 1-4 are same problem solved with different method.)
+
+2. inch to feet calculator and mile to kilometers calculator.
+
+3. Find the given year leap year or not.
+
+4. Wite Fibonacchi series function.
+
+5. Find the largest number from an array of integers.
+
+6. Find max number from an array of integers.
+
+7. Write a function that give the total sum of an array.
